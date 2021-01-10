b33e57ba700a46b5ae5ff5ba8a43e800467fb8e4
azatd <azatdemir40@gmail.com>
2021-01-10 22:09:59 +0300

    Commensis task

--- commencis_task/Tech_Blog_Website.py
+++ commencis_task/Tech_Blog_Website.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 import logging
 
-print()
-
 #Direction of webdriver has designated
 driver = webdriver.Chrome(executable_path=r'D:\Automation\selenium\chromedriver.exe')
 
